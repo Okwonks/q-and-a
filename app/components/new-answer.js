149871acd63bcffd6f3e78dac9924345e09d7ade
Okwonks <albertoketch@gmail.com>
2017-07-24 07:06:27 +0300

    Adding line of code to the .get() that will allow users to give answers or ask questions and they won't have to fill in on the fields that are specified. a blank space will not cause any mixin error.

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -8,10 +8,16 @@ export default Ember.Component.extend({
         },
         saveAnswer() {
             var params = {
-                author: this.get('author'),
-                answered: this.get('answered'),
-                question: this.get('question')
+                author: this.get('author') ? this.get('author') : "Anonymous",
+                answered: this.get('answered') ? this.get('answered') : "Oops, no answer",
+                question: this.get('question'),
+                dateAnswered: new Date().toISOString().substring(0,1)
             };
+            //This will return the input fields to empty
+            this.set('author',  ''); 
+            this.set('answered', '');
+            /* It ends right here ^^ */
+            
             this.set('addNewAnswer', false);
             this.sendAction('saveAnswer', params);
         },

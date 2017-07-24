c83da5bd3e487f497b26593318cc44de9294ddd0
Okwonks <albertoketch@gmail.com>
2017-07-24 07:13:56 +0300

    Adding upvote and downvote to the answer.js model. This specifies what the upvote and downvote will be doing in the application. In this case they are defined as numbers.

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     author: DS.attr(),
     answered: DS.attr(),
+    upvote: DS.attr('number'),
+    downvote: DS.attr('number'),
     question: DS.belongsTo('question', { async: true })
 });
